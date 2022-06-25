4d8860b871b9bcddaa92972cc7e66622af5675fc
LucasAlv3s <lucasbiel40@gmail.com>
2022-06-24 23:15:02 -0300

    cores para a estrutura visual do projeto

---
+++ src/components/Logo.tsx
@@ -0,0 +1,5 @@
+export function Logo() {
+  return (
+    <img width="150" height="35" src="https://www.fatecourinhos.edu.br/ciencia.png" alt="Logo_do_cuso_de_Ciência_de_Dados_da_Fatec_de_Ourinhos" />
+  );
+}
